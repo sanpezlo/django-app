2364574651fdb00ed88851164f33ef63c25fee4b
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2024-03-16 14:07:07 -0500

    feat: add models to admin

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, IdentificationType, Country, Department, City, Person, Student
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(IdentificationType)
+admin.site.register(Country)
+admin.site.register(Department)
+admin.site.register(City)
+admin.site.register(Person)
+admin.site.register(Student)
