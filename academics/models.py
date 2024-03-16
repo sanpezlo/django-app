b59e062e72f03cfdb759a6fef9b844c350570068
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2024-03-16 13:53:44 -0500

    feat: add identification type model

--- academics/models.py
+++ academics/models.py
@@ -8,3 +8,12 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+class IdentificationType(models.Model):
+    name = models.CharField(max_length=50)
+    abrev = models.CharField(max_length=10)
+    descrip = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
