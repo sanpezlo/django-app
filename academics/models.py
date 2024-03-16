095af556267f518bf52a4968893a74b04cf76591
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2024-03-16 13:49:30 -0500

    feat: add user model

--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=250)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
